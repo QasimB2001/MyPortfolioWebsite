0361d52b1f0366185b028bb77e8a99d47cfab8f1
QasimB2001 <QasimB2001@hotmail.co.uk>
2022-08-25 00:33:07 +0100

    Changes to Django
    Added the initial main index.html, css and javascript files

--- MyPortfolioWebsite/MyPortfolioWebsite/urls.py
+++ MyPortfolioWebsite/MyPortfolioWebsite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def say_hello(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', say_hello)
 ]
